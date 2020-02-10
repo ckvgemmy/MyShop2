d962ddf07bdc18ddd57f7f02e0d290f8b55703ab
ckvgemmy <59798528+ckvgemmy@users.noreply.github.com>
2020-02-10 22:17:35 +0800

    Product Category cant add

---
+++ MyShop/MyShop.DataAccess.InMemory/ProductCategoryRepository.cs
@@ -0,0 +1,113 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class ProductCategoryRepository
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<ProductCategory> productCategories;
+
+
+        /// <summary>
+        /// 1.Gotta becareful of the naming convention. ProductCategory in a list is called productCategories. 
+        /// 2.Product in a list is called products
+        /// </summary>
+
+        public ProductCategoryRepository()
+        {
+            productCategories = cache["products"] as List<ProductCategory>;
+            if (productCategories == null)
+            {
+                productCategories = new List<ProductCategory>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache["productCategories"] = productCategories;
+        }
+
+
+
+        //1.in the productrepository you must add the methode to manipulate the data to the database
+        //2.add, delete , edit, find
+
+        public void Insert(ProductCategory p)
+        {
+            productCategories.Add(p);
+        }
+
+
+        /// <summary>
+        /// 1.Update product category - create object type Product Category called productCategoryToUpdate and productCategory.
+        /// 2.Then we need to find the product based on the id. Find the productCategory from looking at the productcategories(list)
+        /// 3.if everything alright, then only assign the value of productCategoryToUpdate to productCategory
+        /// </summary>
+
+        public void Update(ProductCategory productCategory)
+        {
+            ProductCategory productCategoryToUpdate = productCategories.Find(p => p.Id == productCategory.Id);
+
+            if (productCategoryToUpdate != null)
+            {
+                productCategoryToUpdate = productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category not found");
+            }
+
+        }
+
+
+        /// <summary>
+        /// 1.This one , only 1 argument string Id, so find only id.
+        /// 2.Maybe because you dont need to edit it only find
+        /// </summary>
+
+        public ProductCategory Find(string Id)
+        {
+            ProductCategory productCategory = productCategories.Find(p => p.Id == Id);
+
+            if (productCategory != null)
+            {
+                return productCategory;
+            }
+            else
+            {
+                throw new Exception("Product Category not found");
+            }
+        }
+
+
+        //1.iqueryable is suitable for querying data from off memory(database)
+        public IQueryable<ProductCategory> Collection()
+        {
+            return productCategories.AsQueryable();
+
+        }
+
+
+        //1. Delete - no need to define another object because no need to assign new value. Just delete it.
+
+        public void Delete(string Id)
+        {
+            ProductCategory productCategoryToDelete = productCategories.Find(p => p.Id == Id);
+
+            if (productCategoryToDelete != null)
+            {
+                productCategories.Remove(productCategoryToDelete);
+            }
+            else
+            {
+                throw new Exception("Product Category not found");
+            }
+        }
+    }
+}
---
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryManagerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyShop.Core.Models;
+using MyShop.DataAccess.InMemory;
+
+namespace MyShop.WebUI.Controllers
+{
+    public class ProductCategoryManagerController : Controller
+    {
+        ProductCategoryRepository context;
+
+        public ProductCategoryManagerController()
+        {
+            context = new ProductCategoryRepository();
+        }
+
+
+
+        /// <summary>
+        /// 1.this is the default action, index. usually list all products
+        /// 2.here we get the list of products from repository then equate it to our context of type productrepository
+        /// 3. then we list all the products
+        /// </summary>
+
+
+        public ActionResult Index()
+        {
+            List<ProductCategory> productCategories = context.Collection().ToList();
+            return View(productCategories);
+        }
+
+        public ActionResult Create()
+        {
+            ProductCategory productCategory = new ProductCategory();
+            return View(productCategory);
+        }
+
+        [HttpPost]
+        public ActionResult Create(ProductCategory productCategory)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(productCategory);
+            }
+            else
+            {
+                context.Insert(productCategory);
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            ProductCategory productCategory = context.Find(Id);
+            if (productCategory == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productCategory);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product productCategory, string Id)
+        {
+            ProductCategory productCategoryToEdit = context.Find(Id);
+
+            if (productCategoryToEdit == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if (!ModelState.IsValid)
+                {
+                    return View(productCategory);
+                }
+
+                productCategoryToEdit.Category = productCategory.Category;
+
+
+                context.Commit();
+
+                return RedirectToAction("Index");
+            }
+        }
+
+        public ActionResult Delete(Product product, string Id)
+        {
+            ProductCategory productTodelete = context.Find(Id);
+
+            if (productTodelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(productTodelete);
+            }
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult ConfirmDelete(string Id)
+        {
+            ProductCategory productTodelete = context.Find(Id);
+
+            if (productTodelete == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Delete(Id);
+                context.Commit();
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
